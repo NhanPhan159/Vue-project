78cd4e54c001c1c137adf2b9f81be934d707a113
NhanPhan159 <nhandut2019@gmail.com>
2022-12-11 10:30:08 +0700

    For deloy

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{info}}
+    <el-button type="primary">Primary</el-button>
+    <i class="el-icon-edit"></i>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default{ 
+  data() {
+    return{
+      info : null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
